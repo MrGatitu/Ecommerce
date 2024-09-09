96cbf312fa7d58f3e21a27e0b920ec8abb20e195
MrGatitu <mrgatituh@gmail.com>
2024-09-09 14:31:56 +0300

    Products conroller

---
+++ BackEnd/OnlineShop/src/main/java/com/example/OnlineShop/ProductsController.java
@@ -0,0 +1,12 @@
+package com.example.OnlineShop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/products")
+public class ProductsController {
+    // @GetMapping
+
+}
