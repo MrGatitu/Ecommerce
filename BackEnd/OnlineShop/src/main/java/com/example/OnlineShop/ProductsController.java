a8d951e75b38887bdf823932c2706c0a6b6527d3
MrGatitu <mrgatituh@gmail.com>
2024-09-09 14:48:06 +0300

    updated products controller

--- BackEnd/OnlineShop/src/main/java/com/example/OnlineShop/ProductsController.java
+++ BackEnd/OnlineShop/src/main/java/com/example/OnlineShop/ProductsController.java
@@ -7,6 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/products")
 public class ProductsController {
-    // @GetMapping
+
+    private final ProductRepository productRepository;
+
+    public ProductsController(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+    @GetMapping("All")
+    public Iterable<Integer> findAllProducts(){
+        return this.productRepository.findAll();
+    }
 
 }
