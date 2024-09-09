79bea330e1e33d92933525f88bdb7fc2c67adf35
MrGatitu <mrgatituh@gmail.com>
2024-09-09 16:20:01 +0300

    Added Controller Methods 'GET"

--- BackEnd/OnlineShop/src/main/java/com/example/OnlineShop/ProductsController.java
+++ BackEnd/OnlineShop/src/main/java/com/example/OnlineShop/ProductsController.java
@@ -1,5 +1,6 @@
 package com.example.OnlineShop;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,18 @@ public class ProductsController {
 
     private final ProductRepository productRepository;
 
+    // @Autowired
     public ProductsController(ProductRepository productRepository){
         this.productRepository = productRepository;
     }
     @GetMapping("All")
-    public Iterable<Integer> findAllProducts(){
+    public Iterable<Products> findAllProducts(){
         return this.productRepository.findAll();
+    }
+
+    @GetMapping("Hello")
+    public String Hello(){
+        return "Hello, Welcomes to this Repository";
     }
 
 }
